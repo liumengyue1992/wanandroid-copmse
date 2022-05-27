7680690ad638e8ffbe691169df62b50b876e0b37
mengyue.liu <mengyue.liu@upuphone.com>
2022-05-27 18:04:39 +0800

    首页导航构建：使用Scaffold+BottomNavigation

--- app/src/main/java/com/lmy/wanandroid_compose/ui/main/Main.kt
+++ app/src/main/java/com/lmy/wanandroid_compose/ui/main/Main.kt
@@ -1,7 +1,9 @@
 package com.lmy.wanandroid_compose.ui.main
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,6 +29,8 @@ import com.lmy.wanandroid_compose.ui.home.Home
 import com.lmy.wanandroid_compose.ui.mine.Mine
 import com.lmy.wanandroid_compose.ui.project.Project
 import com.lmy.wanandroid_compose.ui.square.Square
+import com.lmy.wanandroid_compose.ui.theme.TabSelect
+import com.lmy.wanandroid_compose.ui.theme.TabUnSelect
 
 /**
  * @description：首页
@@ -37,15 +42,21 @@ import com.lmy.wanandroid_compose.ui.square.Square
 @Composable
 fun Main() {
     val navController = rememberNavController()
-    val bottomItems = listOf(Route.Home, Route.Square, Route.Project, Route.Mine)
-    // tab未选图标
-    val icons = listOf(R.mipmap.home,R.mipmap.home,R.mipmap.home,R.mipmap.home)
+    val bottomItems = listOf(Route.Home, Route.Square, Route.Wechat, Route.Mine)
     // tab选中图标
-    // val selectIcons = listOf(R.drawable.homeselect,R.drawable.findselect,R.drawable.profileselect)
+    val selectIcons = listOf(R.drawable.ic_tab_home_fill,
+        R.drawable.ic_tab_square_fill,
+        R.drawable.ic_tab_wechat_fill,
+        R.drawable.ic_tab_my_fill)
+    // tab未选中图标
+    val icons = listOf(R.drawable.ic_tab_home,
+        R.drawable.ic_tab_square,
+        R.drawable.ic_tab_wechat,
+        R.drawable.ic_tab_my)
     var selectIndex by remember { mutableStateOf(0) }
     Scaffold(modifier = Modifier.fillMaxSize(),
         bottomBar = {
-            BottomNavigation(backgroundColor = Color.Blue) {
+            BottomNavigation(backgroundColor = Color.White, elevation = 16.dp) {
                 // 获取当前的 NavBackStackEntry,此条目允许您访问当前的 NavDestination
                 // 通过 hierarchy 辅助方法将该项的路由与当前目的地及其父目的地的路由进行比较来确定每个 BottomNavigationItem 的选定状态（以处理使用嵌套导航的情况）。
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
@@ -57,7 +68,8 @@ fun Main() {
                             // 根据routeName导航到指定页面
                             navController.navigate(route.routeName) {
                                 //当底部导航导航到在非首页的页面时，执行手机的返回键回到首页
-                                popUpTo(navController.graph.startDestDisplayName)
+                                popUpTo(navController.graph.startDestDisplayName){
+                                }
                                 // 避免重新选择同一个导航
                                 launchSingleTop = true
                                 // Restore state when reselecting a previously selected item
@@ -66,11 +78,15 @@ fun Main() {
                         },
                         icon = {
                             when (index) {
-                                selectIndex -> Icon(painter = painterResource(icons[index]), contentDescription = "")
-                                else -> {
-                                }
+                                selectIndex -> Icon(painter = painterResource(selectIcons[index]),
+                                    contentDescription = "icon",
+                                    modifier = Modifier.size(25.dp),
+                                    tint = TabSelect)
+                                else -> Icon(painter = painterResource(icons[index]),
+                                    contentDescription = "icon",
+                                    modifier = Modifier.size(25.dp),
+                                    tint = TabUnSelect)
                             }
-                            
                         },
                         label = {
                             Text(text = stringResource(id = route.resId))
@@ -85,7 +101,7 @@ fun Main() {
                 composable(Route.Square.routeName) {
                     Square(navController)
                 }
-                composable(Route.Project.routeName) {
+                composable(Route.Wechat.routeName) {
                     Project(navController)
                 }
                 composable(Route.Mine.routeName) {
@@ -104,6 +120,6 @@ fun Main() {
 sealed class Route(val routeName: String, @StringRes val resId: Int) {
     object Home : Route("home", R.string.home)
     object Square : Route("square", R.string.square)
-    object Project : Route("project", R.string.project)
+    object Wechat : Route("wechat", R.string.wechat)
     object Mine : Route("mine", R.string.mine)
 }
